19d3578a432aa9c459256034c61a89375c84db04
Sai nagaraju yarramsetti <122505881+NAGARAJU-YARRAMSETTI@users.noreply.github.com>
2023-07-03 09:41:54 +0530

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnetapp.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace dotnetapp.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private BusinessLayer bussiness_layer = new BusinessLayer();
+
+        [HttpPost("user/addProfile")]
+        public string addUser(ProfileModel pm)
+        {
+            return bussiness_layer.addUser(pm);
+        }
+
+        [HttpGet("user/getProfile/{userId}")]
+        public IActionResult getUser(string userId)
+        {
+            return Ok(bussiness_layer.getUser(userId));
+        }
+
+
+        [HttpPut("user/editProfile/{userId}")]
+        public string editUser(string userId, ProfileModel user)
+        {
+            return bussiness_layer.editUser(userId, user);
+        }
+
+        [HttpDelete("user/deleteProfile/{userId}")]
+        public string deleteUser(string userId)
+        {
+            return bussiness_layer.deleteUser(userId);
+        }
+
+    }
+}
+
+
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace bikeloan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
